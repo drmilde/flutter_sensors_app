5b747181c2967603a04afc8dbfe7a13270cdc55e
drmilde <milde@hs-fulda.de>
2020-01-28 14:08:10 +0100

    Sensoren eingebaut

--- lib/screens/display_sensor_screen.dart
+++ lib/screens/display_sensor_screen.dart
@@ -40,12 +40,12 @@ class _DisplaySensorScreenState extends State<DisplaySensorScreen> {
 // [AccelerometerEvent (x: 0.0, y: 9.8, z: 0.0)]
 
     userAccelerometerEvents.listen((UserAccelerometerEvent event) {
-      print(event);
+      //print(event);
     });
 // [UserAccelerometerEvent (x: 0.0, y: 0.0, z: 0.0)]
 
     gyroscopeEvents.listen((GyroscopeEvent event) {
-      print(event);
+      //print(event);
     });
   }
 }
